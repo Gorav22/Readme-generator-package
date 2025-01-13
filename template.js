aa2a37b6dea533b81c8891c16ce57a0e9517b327
Gorav <goravjindal86@gmail.com>
2025-01-13 17:05:14 +0530

    npm package

---
+++ template.js
@@ -0,0 +1,50 @@
+function generateReadmeTemplate({
+    projectName,
+    description,
+    preRequisites,
+    installationSteps,
+    environmentVariables,
+    techStack,
+    features,
+    contributing,
+    license,
+  }) {
+    return `
+  # ${projectName}
+  
+  ## Introduction
+  ${description}
+  
+  ## Setup and Installation
+  
+  ### Pre-Requisites
+  ${preRequisites.join("\n")}
+  
+  ### Installation
+  ${installationSteps}
+  
+  ### Environment Variables
+  The project relies on several environment variables. Create a \`.env\` file with the following variables:
+  
+  ${environmentVariables}
+  
+  ## Tech Stack
+  ### Frontend
+  ${techStack.frontend.join("\n")}
+  
+  ### Backend
+  ${techStack.backend.join("\n")}
+  
+  ## Features
+  ${features.join("\n")}
+  
+  ## Contributing
+  ${contributing}
+  
+  ## License
+  ${license}
+    `;
+  }
+
+module.exports = { generateReadmeTemplate };
+  
